30502a566f07923e41152781642e4540a762efb9
SJZeng <sjzeng.tw@gmail.com>
2017-07-13 01:43:32 +0800

    add production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
 
+var inProduction = (process.env.NODE_ENV === 'production');
+
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -15,5 +17,12 @@ module.exports = {
         loader: "babel-loader" 
       }
     ]
-  }
+  },
+  plugins: []
 };
+
+if (inProduction) {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
