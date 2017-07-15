27956db0cfd3790f4bf950d2c9a3aac6b89b0e6b
SJZeng <sjzeng.tw@gmail.com>
2017-07-16 02:34:05 +0800

    using raw-loader to ignore url

--- webpack.config.js
+++ webpack.config.js
@@ -19,13 +19,7 @@ module.exports = {
       {
         test: /\.s[ac]ss$/,
         use: ExtractTextPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {url: false}
-            },
-            'sass-loader'
-          ],
+          use: ['raw-loader', 'sass-loader'],
           fallback: 'style-loader'
         })
       },
